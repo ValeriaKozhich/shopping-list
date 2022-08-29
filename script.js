4f88d8393ef5a8ccea45e6ae53f93e4b0a2e7100
ValeriaKozhich <kozhichvl@mail.ru>
2022-08-29 21:05:26 +0300

    Д/з 4

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 /* Новые элементы должны добавляться в список по нажатию на Enter */
+const input = document.querySelector ('#input');
+const groceries = document.querySelector ('.groceries');
 
-/* Пустые элементы не должны добавляться */
+input.addEventListener ('keydown', function(event) {
+    if (event.key == 'Enter') {
+        //Отправка сообщения
+    }
+    const itemsText = input.value;
+
+    const items = document.createElement ('.items');
+
+    /* Пустые элементы не должны добавляться */
+    if (itemsText != ' ') {
+        groceries.append(items);
+    }
+    
+    /* Очищать input после добавления нового элемента в список */
+    input.value = ' ';
+});
 
 /* Если кликнуть на элемент списка, он зачеркивается */
 
 /* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
 
-/* Очищать input после добавления нового элемента в список */
+
