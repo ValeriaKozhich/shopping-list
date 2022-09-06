fadd61dea76333025d27950304d46b127f97302f
ValeriaKozhich <kozhichvl@mail.ru>
2022-09-06 23:28:01 +0300

    ДЗ 4

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
 /* Новые элементы должны добавляться в список по нажатию на Enter */
 const input = document.querySelector ('#input');
-const groceries = document.querySelector ('.groceries');
+const groceries = document.querySelector ('.items');
 
 input.addEventListener ('keydown', function(event) {
     if (event.key == 'Enter') {
         //Отправка сообщения
-    }
-    const itemsText = input.value;
+        const itemsText = input.value;
 
-    const items = document.createElement ('.items');
+        const items = document.createElement ('div');
+        items.textContent = itemsText;
+    };
+    
+});
 
+    
     /* Пустые элементы не должны добавляться */
-    if (itemsText != ' ') {
-        groceries.append(items);
-    }
+if (itemsText != '') {
+    groceries.append(items);
+};
     
     /* Очищать input после добавления нового элемента в список */
-    input.value = ' ';
-});
+input.value = '';
 
 /* Если кликнуть на элемент списка, он зачеркивается */
 
-/* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
 
+item.addEventListener ('click', cross) 
+
+    function cross() {
+      item.classList.toggle('done')
+    };
+/* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
 
